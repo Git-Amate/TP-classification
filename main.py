831d9d1a49bd4bdae4a87c8d091804eb11784a69
Mamadou Amate FALL <amatedu93@air-de-mamadou.home>
2024-04-08 22:51:13 -0400

    ajout pre-traitement de texte avec nltk

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import os
+import seaborn as sns
 import re
 from sklearn.inspection import DecisionBoundaryDisplay
 import numpy as np
+import matplotlib.pyplot as plt
 from matplotlib import colors
+import nltk
+import ssl
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import string
 from sklearn.svm import SVC
 from sklearn.decomposition import TruncatedSVD
 import matplotlib.pyplot as plt
@@ -12,14 +23,25 @@ from pandas import *
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score
+from sklearn.metrics import accuracy_score, precision_score, f1_score , confusion_matrix
+
+
 
 # Liste pour stocker les données de chaque fichier
 donnees = []
 avis = []
 
-columns_name = ['commentaire', 'type_de_commentaire']
 #chemin = "txt_sentoken/neg"
+
+
+def pretraitement_texte(texte):
+    tokens = word_tokenize(texte)
+    tokens = [mot for mot in tokens if mot not in string.punctuation]
+    mots_vides = set(stopwords.words('english'))
+    tokens = [mot for mot in tokens if mot.lower() not in mots_vides]
+    lemmatiseur = WordNetLemmatizer()
+    tokens = [lemmatiseur.lemmatize(mot) for mot in tokens]
+    return " ".join(tokens)
 
 def getData(chemin,type_commentaire):
     for fichier in os.listdir(chemin):
@@ -35,10 +57,15 @@ getData("txt_sentoken/neg","neg")
 getData("txt_sentoken/pos","pos")
 
 
+
 data = DataFrame({"commentaire": donnees, "nature_du_commentaire": avis})
 # Mélanger le DataFrame
 data = data.sample(frac=1)
 data.reset_index(drop=True, inplace=True)
+data['commentaire'] = data['commentaire'].apply(pretraitement_texte)
+print(data["commentaire"].head())
+#print(len(data))
+#print(data['nature_du_commentaire'].value_counts())
 #print(data.head())
 
 # X valeur d'apprentissage
@@ -49,17 +76,26 @@ y = data["nature_du_commentaire"]
 
 # entrainement
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-
-
+print(X_train.head())
 
 vectorizer = CountVectorizer(lowercase=True)
 X_train_vectorized = vectorizer.fit_transform(X_train)
+#print(X_train_vectorized)
 #print(vectorizer.vocabulary_)
 X_test_vectorized = vectorizer.transform(X_test)
 
 '''
+# Récupération du vocabulaire
+vocab = vectorizer.get_feature_names_out()
+for i, row in enumerate(X_train_vectorized.toarray()):
+    print("Document", i+1, ":")
+    for j, count in enumerate(row):
+        if count > 0:
+            print(vocab[j], ":", count)
+'''
+
 #Instancier le modèle SVM
-svm_classifier = SVC()
+svm_classifier = SVC(kernel='linear')
 
 #Entraîner le modèle sur les données d'entraînement normalisées
 svm_classifier.fit(X_train_vectorized, y_train)
@@ -77,22 +113,35 @@ naive_bayes_classifier.fit(X_train_vectorized.toarray(), y_train)
 
 # Faire des prédictions sur l'ensemble de test normalisé
 y_pred = naive_bayes_classifier.predict(X_test_vectorized.toarray())
+'''
 
 #Évaluer la performance du modèle
+f1 = f1_score(y_test, y_pred, pos_label="pos")
 accuracy = accuracy_score(y_test, y_pred)
 precision = precision_score(y_test, y_pred,  pos_label="pos")
+confusion_matrix = confusion_matrix(y_test,y_pred)
+print("f1_score:", f1)
 print("Accuracy:", accuracy)
 print("Precision:", precision)
+print("confusion_matrix:", confusion_matrix)
 
-vectorizer = CountVectorizer()
-X_vectorized = vectorizer.fit_transform(X)
+'''
 pca = PCA(n_components=2)
-X_reduced = pca.fit_transform(X_vectorized.toarray())
+X_train_pca = pca.fit_transform(X_train_vectorized.toarray())
 
-disp = DecisionBoundaryDisplay.from_estimator(
-    naive_bayes_classifier, X_reduced, response_method="predict", xlabel="message", ylabel=type,alpha=0.5 )
+plt.figure(figsize=(10, 8))
+# Plotting our two-features-space
+sns.scatterplot(x=X_train_pca[:, 0],
+                y=X_train_pca[:, 1],
+                hue=y_train,
+                s=8)
+# Constructing a hyperplane using a formula.
+w = svm_classifier.coef_[0]           # w consists of 2 elements
+b = svm_classifier.intercept_[0]      # b consists of 1 element
+x_points = np.linspace(-1, 1)    # generating x-points from -1 to 1
+y_points = -(w[0] / w[1]) * x_points - b / w[1]  # getting corresponding y-points
+# Plotting a red hyperplane
+plt.plot(x_points, y_points, c='r');
+'''
 
-# Vectorisation des textes
-disp.ax_.scatter(X_reduced[:, 0], X_reduced[:, 1], c=y , edgecolor="k")
-plt.show()
 
